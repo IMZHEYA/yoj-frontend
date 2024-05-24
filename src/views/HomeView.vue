21812ca130b602d64494c787d0430f9ded5a3bc5
Serendipityzhezi <1342558165@qq.com>
2024-05-24 15:15:48 +0800

    feat:😀整合markdown编辑器

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
+    <MdEditor :value="value" :handle-change="onChange" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; 
+<script setup lang="ts">
+import { ref } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import MdEditor from "@/components/MdEditor.vue";
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+const value = ref();
+
+const onChange = (v: string) => {
+  value.value = v;
+};
 </script>
+<style>
+.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child {
+  display: none;
+}
+</style>
