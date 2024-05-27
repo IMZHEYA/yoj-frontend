e05cf200c8bc9be64e9f0ab614e31ec2255f76f3
Serendipityzhezi <1342558165@qq.com>
2024-05-27 20:30:36 +0800

    fix：😉修改代码编辑器相关代码

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,21 +1,64 @@
 <template>
-  <div id="code-editor" ref="codeEditorRef" style="min-height: 400px" />
+  <div
+    id="code-editor"
+    ref="codeEditorRef"
+    style="min-height: 600px"
+    height="70vh"
+  />
 </template>
 <script setup lang="ts">
 import * as monaco from "monaco-editor";
-import { onMounted, ref, toRaw } from "vue";
+import { onMounted, ref, toRaw, watch } from "vue";
 
-const codeEditorRef = ref();//创建了一个 Monaco Editor 实例。
+const codeEditorRef = ref(); //创建了一个 Monaco Editor 实例。
 const codeEditor = ref(); //用于存储 Monaco Editor 实例。
 const value = ref("hello world");
 
-onMounted(() =>{
-  if(!codeEditorRef.value){
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string;
+  language?: string;
+  handleChange: (v: string) => void;
+}
+/**
+ * 给组件指定初始值
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  language: () => "java",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+// watch(
+//   () => props.language,
+//   () => {
+//     codeEditor.value = monaco.editor.create(codeEditorRef.value, {
+//       value: value.value,
+//       language: props.language,
+//       automaticLayout: true,
+//       colorDecorators: true,
+//       minimap: {
+//         enabled: true,
+//       },
+//       readOnly: false,
+//       theme: "vs-dark",
+//       // lineNumbers: "off",
+//       // roundedSelection: false,
+//       // scrollBeyondLastLine: false,
+//     });
+//   }
+// );
+
+onMounted(() => {
+  if (!codeEditorRef.value) {
     return;
   }
-  codeEditor.value = monaco.editor.create(codeEditorRef.value,{
+  codeEditor.value = monaco.editor.create(codeEditorRef.value, {
     value: value.value,
-    language: "java",
+    language: props.language,
     automaticLayout: true,
     colorDecorators: true,
     minimap: {
@@ -29,26 +72,8 @@ onMounted(() =>{
   });
   // 编辑 监听内容变化
   codeEditor.value.onDidChangeModelContent(() => {
-     console.log("目前内容为：", toRaw(codeEditor.value).getValue());
+    props.handleChange(toRaw(codeEditor.value).getValue());
   });
-});
-
-
-/**
- * 定义组件属性类型
- */
-interface Props {
-  value: string;
-  handleChange: (v: string) => void;
-}
-/**
- * 给组件指定初始值
- */
-const props = withDefaults(defineProps<Props>(), {
-  value: () => "",
-  handleChange: (v: string) => {
-    console.log(v);
-  },
 });
 </script>
 
---
+++ src/components/MdViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <Viewer :value="value" :plugins="plugins" />
+</template>
+<script setup lang="ts">
+import { Editor, Viewer } from "@bytemd/vue-next";
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+const plugins = [gfm(), highlight()];
+/**
+ * 定义组件属性类型
+ */
+interface Props {
+  value: string;
+}
+/**
+ * 给组件指定初始值
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+});
+</script>
+
+<style></style>
